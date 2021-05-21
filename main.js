858a10ae7ea093d9b860c5265635a9347e088aa2
Dalen Chan <dalenchan@dalens-mbp.lan>
2021-05-21 04:55:11 +0100

    Quote Printing

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // const axios = require('axios').default;
     
 function getRequest(){
-    console.log('hello');
         const options = {
     method: 'GET',
     url: 'http://tronalddump.io/random/quote'
     };
 
     axios.request(options).then(function (response) {
-        console.log(response.data);
         let parsedQuote = response.data.value;
         console.log(parsedQuote);
+        var quote = document.getElementById('quote');
+        quote.innerText = "\"" + parsedQuote + "\"";
     }).catch(function (error) {
         console.error(error);
     });
